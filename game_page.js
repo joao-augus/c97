be6caf89c4008fce7edd83d45d480a5f32e88abb
joao-augus <152932797+joao-augus@users.noreply.github.com>
2024-04-15 19:57:24 -0300

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,7 @@
+    /* variaveis para armazenar os nomes dos jogadores */
+    player1_name = localStorage.getItem("player1_name");
+    player2_name = localStorage.getItem("player2_name");
+    
+    /* variaveis para armazenar os pontos dos jogadores */
+    player1_score =0;
+    player2_score =0;
